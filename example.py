7c02047c3b2b36efeab5bdad39bf244632dca9f0
jansonkhadarvalli <Johnsongummineni@gmail.com>
2021-08-18 12:57:28 +0530

    logic is added

--- example.py
+++ example.py
@@ -1,4 +1,10 @@
-x = 5
-y = 6
+x = int(input('Enter a number'))
+y = int(input('Enter another number'))
 z = x+y
 print(z)
+if z>10:
+    print('sum is greater than 10')
+else:
+    print('sum is less than 10')
+
+
